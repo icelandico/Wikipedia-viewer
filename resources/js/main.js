e592a52a7657481a84a61d7666483198fe55eed8
Michal Muszynski <m.muszy@gmail.com>
2018-07-15 22:20:24 +0200

    Refinement

--- resources/js/main.js
+++ resources/js/main.js
@@ -19,15 +19,14 @@ btnSrch.addEventListener('click', parse);
 selectedLimit.addEventListener('change', changeArticlesLimit);
 
 function parse() {
-    console.log(wikiApiSite);
-    unorderedList.innerHTML = '';
-    var url = wikiApiSite + input.value;
-    wikiApi.open('GET', url, true);
-    wikiApi.onload = function () {
-        data = JSON.parse(this.response);
-        getValues(data);
-    };
-    wikiApi.send();
+  unorderedList.innerHTML = '';
+  var url = wikiApiSite + input.value;
+  wikiApi.open('GET', url, true);
+  wikiApi.onload = function () {
+      data = JSON.parse(this.response);
+      getValues(data);
+  };
+  wikiApi.send();
 }
 
 function randomArticle() {
@@ -35,22 +34,22 @@ function randomArticle() {
 }
 
 function getValues(item) {
-    var heading = item[1];
-    var content = item[2];
-    var link = item[3];
-    for (var i = 0; i < heading.length; i++) {
-        createListElem(heading[i], content[i], link[i])
-    }
+  var heading = item[1];
+  var content = item[2];
+  var link = item[3];
+  for (var i = 0; i < heading.length; i++) {
+      createListElem(heading[i], content[i], link[i])
+  }
 };
 
 function createListElem(heading, content, link) {
-    var newDiv = '<li class="results-item">' +
-                    '<a href=' + link  + ' target="_blank">'+
-                        '<h2>' + heading + '</h2>' +
-                        '<p>' + content + '</p>' +
-                    '</a>' +
-                '</li>';
-    unorderedList.insertAdjacentHTML("beforeend", newDiv);
+  var newDiv = '<li class="results-item">' +
+                  '<a href=' + link  + ' target="_blank">'+
+                      '<h2>' + heading + '</h2>' +
+                      '<p>' + content + '</p>' +
+                  '</a>' +
+              '</li>';
+  unorderedList.insertAdjacentHTML("beforeend", newDiv);
 }
 
 function changeArticlesLimit() {
